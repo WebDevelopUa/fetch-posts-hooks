c7d9cbcc47571f867c7e381e5b8f098cb4544e90
WebDevelopUa <js.develop.com@gmail.com>
2020-05-29 00:45:59 +0000

    Updates

---
+++ precache-manifest.3cfed7694dd65fd00fa71761898654d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddb0b561438ac3e8e8f621d3a90c15b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8bc2d2fe1340e483debc",
+    "url": "./static/js/2.a85202e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a85202e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19bea4564b879ef9f93",
+    "url": "./static/js/main.d3a7a8fa.chunk.js"
+  },
+  {
+    "revision": "fcfd876978108fbec6c0",
+    "url": "./static/js/runtime-main.cc1bb171.js"
+  }
+]);
